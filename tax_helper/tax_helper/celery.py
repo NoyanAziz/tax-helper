0ba0df613fadede9ddf4c9812149635849fba5d4
Muhammad Noyan  Aziz <noyan.aziz>
2024-05-01 09:40:21 +0500

    fix: update celery for deployment

--- tax_helper/tax_helper/celery.py
+++ tax_helper/tax_helper/celery.py
@@ -1,17 +1,15 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_helper.settings')
 
-app = Celery('tax_helper')
+BROKER_HOST = os.environ.get('BROKER_HOST', None)
+if BROKER_HOST is None:
+    raise Exception("BROKER_HOST environmental variable is not defined")
+broker_url = 'pyamqp://guest@' + BROKER_HOST + '//'
+
+app = Celery('tasks', broker=broker_url, backend='rpc://')
 
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
-
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    """Debug task."""
-    print(f'Request: {self.request!r}')
+@app.task
+def add(x, y):
+    return x + y
