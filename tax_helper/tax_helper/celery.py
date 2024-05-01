f2c11d6090555c7d6f65cc15cae90f1d81afdb13
Muhammad Noyan  Aziz <noyan.aziz>
2024-05-01 10:29:27 +0500

    fix: celery str

--- tax_helper/tax_helper/celery.py
+++ tax_helper/tax_helper/celery.py
@@ -1,6 +1,8 @@
 import os
 from celery import Celery
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_helper.settings')
+
 BROKER_HOST = os.environ.get('BROKER_HOST', None)
 if BROKER_HOST is None:
     raise Exception("BROKER_HOST environmental variable is not defined")
@@ -16,5 +18,5 @@ app.autodiscover_tasks()
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
-    """Debug task."""
+    """Debug task"""
     print(f'Request: {self.request!r}')
