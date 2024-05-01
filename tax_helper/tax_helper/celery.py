8b1ee8bb78f63fe0366a8ab0b86e98de49fc2f07
Muhammad Noyan  Aziz <noyan.aziz>
2024-05-01 10:08:04 +0500

    fix: celery

--- tax_helper/tax_helper/celery.py
+++ tax_helper/tax_helper/celery.py
@@ -1,15 +1,20 @@
 import os
 from celery import Celery
 
-
 BROKER_HOST = os.environ.get('BROKER_HOST', None)
 if BROKER_HOST is None:
     raise Exception("BROKER_HOST environmental variable is not defined")
+
 broker_url = 'pyamqp://guest@' + BROKER_HOST + '//'
+app = Celery('task_helper', broker=broker_url, backend='rpc://')
 
-app = Celery('tasks', broker=broker_url, backend='rpc://')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
 
 
-@app.task
-def add(x, y):
-    return x + y
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    """Debug task."""
+    print(f'Request: {self.request!r}')
